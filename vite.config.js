504206de723fed52a9b43beccaffa9e65b51d23d
adejumobioluwafemi <olufemiphil7@gmail.com>
2025-11-12 11:40:15 +0100

    ar not supported error fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     host: true, // Listen on all addresses
     port: 5173
   },
+  publicDir: 'public',
   build: {
     target: 'esnext' // Important for Three.js and modern features
   }
-})
+});
