c7330d8f1b406bbc94843a526365148b2975f1fc
adejumobioluwafemi <olufemiphil7@gmail.com>
2025-11-13 18:53:25 +0100

    changed from pose detector to hand

--- main.js
+++ main.js
@@ -154,13 +154,13 @@ async function setupPoseLandmarker() {
 // Hand Landmarker setup
 async function setupHandLandmarker() {
     const vision = await FilesetResolver.forVisionTasks(
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
     );
 
     handLandmarker = await HandLandmarker.createFromOptions(vision, {
         baseOptions: {
             modelAssetPath:
-                "hand_landmarker.task",
+                "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
         },
         runningMode: "VIDEO",
         numHands: 2,
